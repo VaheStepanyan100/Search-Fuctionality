261b921db4666f5e3f03bbbcad4b3b338348bcdb
VaheStepanyan100 <vahestepanyan100@gmail.com>
2022-10-01 22:30:29 +0400

    First-search-dropdown-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { countries } from './countries';
 
 function App() {
+  const [query, setQuery] = useState('');
+
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <input
+        className='search'
+        type="text"
+        placeholder='Search...'
+        onChange={handleChange}
+      />
+      <ul className='list'>
+        {countries.filter(c => c.country.toLowerCase().includes(query))
+          .map(c => (
+            <li key={c.id}>{c.flag} {c.country}</li>
+          ))}
+      </ul>
     </div>
+
   );
 }
 
---
+++ src/countries.js
@@ -0,0 +1,7 @@
+export const countries = [
+    { country: 'Argentina', flag: '🇦🇷', id: 1 },
+    { country: 'Armenia', flag: '🇦🇲', id: 2 },
+    { country: 'Aruba', flag: '🇦🇼', id: 3 },
+    { country: 'Barbados', flag: '🇧🇧', id: 4 },
+    { country: 'Belarus', flag: '🇧🇾', id: 5 },
+]
